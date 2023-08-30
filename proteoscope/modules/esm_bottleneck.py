97dc5b8de6223deaeac3900813603699dc04be06
sofroniewn <sofroniewn@gmail.com>
2023-08-30 20:58:41 +0000

    update bottleneck

--- proteoscope/modules/esm_bottleneck.py
+++ proteoscope/modules/esm_bottleneck.py
@@ -9,36 +9,73 @@ class ESMBottleneck(nn.Module):
     ):
         super(ESMBottleneck, self).__init__()
 
-        self.embedding = nn.Embedding(1, config.d_model)
-        self.projection = nn.Linear(config.d_init, config.d_model)
+        self.guidance_embedding = nn.Embedding(1, config.d_model)
 
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=config.d_model,
-            nhead=config.nhead,
-            dropout=config.dropout,
-            dim_feedforward=config.dim_feedforward,
-            batch_first=True,
-            norm_first=True,
-        )
-        encoder_norm = nn.LayerNorm(config.d_model)
-        self.encoder = nn.TransformerEncoder(
-            encoder_layer, config.num_encoder_layers, encoder_norm
-        )
+        if config.d_model > 0:
+            self.projection = nn.Linear(config.d_init, config.d_model)
+        else:
+            self.projection = None
 
-    def forward(self, seqs, mask):
-        # project
-        projected = self.projection(seqs)
+        if config.num_encoder_layers > 0:
+            encoder_layer = nn.TransformerEncoderLayer(
+                d_model=config.d_model,
+                nhead=config.nhead,
+                dropout=config.dropout,
+                dim_feedforward=config.dim_feedforward,
+                batch_first=True,
+                norm_first=True,
+            )
+            encoder_norm = nn.LayerNorm(config.d_model)
+            self.encoder = nn.TransformerEncoder(
+                encoder_layer, config.num_encoder_layers, encoder_norm
+            )
+        else:
+            self.encoder = None
 
-        # add guidance token
-        guidance_token = self.embedding(
-            torch.zeros((seqs.shape[0], 1), dtype=int, device=projected.device)
-        )
-        projected = torch.cat([guidance_token, projected], dim=1)
-        guidance_mask = torch.ones((mask.shape[0], 1), dtype=bool, device=mask.device)
-        mask = torch.cat([guidance_mask, mask], dim=1)
+        self.return_type = config.return_type
+        self.unconditioned_type = config.unconditioned_type
 
-        # pass through transformer
-        transformed = self.encoder(projected, src_key_padding_mask=~mask)
+    def forward(self, seqs, mask, unconditioned=False):
+        if unconditioned and self.unconditioned_type == 'token':
+            guidance_token = self.guidance_embedding(
+                torch.zeros((seqs.shape[0], 1), dtype=int, device=seqs.device)
+            )
+            mask = torch.ones((mask.shape[0], 1), dtype=bool, device=mask.device)
+            return guidance_token, mask
+        elif unconditioned and self.unconditioned_type == 'zeros':
+            seqs = torch.zeros((seqs.shape[0], 1), dtype=float, device=seqs.device)
+            mask = torch.ones((mask.shape[0], 1), dtype=bool, device=mask.device)
+            return seqs, mask
+        elif unconditioned:
+            raise ValueError(f'Unrecognized unconditioned type {self.unconditioned_type}')
 
-        # Take just guidence token
-        return transformed[:, 0].unsqueeze(dim=1)
+        if self.projection is not None:
+            # project
+            seqs = self.projection(seqs)
+
+        if self.return_type == 'token':
+            # add guidance token
+            guidance_token = self.guidance_embedding(
+                torch.zeros((seqs.shape[0], 1), dtype=int, device=seqs.device)
+            )
+            seqs = torch.cat([guidance_token, seqs], dim=1)
+            guidance_mask = torch.ones((mask.shape[0], 1), dtype=bool, device=mask.device)
+            mask = torch.cat([guidance_mask, mask], dim=1)
+
+        if self.encoder is not None:
+            # pass through transformer
+            seqs = self.encoder(seqs, src_key_padding_mask=~mask)
+
+        if self.return_type == 'token':
+            # Take just guidence token
+            mask = torch.ones((mask.shape[0], 1), dtype=bool, device=mask.device)
+            return seqs[:, 0].unsqueeze(dim=1), mask
+        elif self.return_type == 'mean':
+            # Take the mean
+            mask = torch.ones((mask.shape[0], 1), dtype=bool, device=mask.device)
+            return seqs.mean(dim=1, keepdim=True), mask
+        elif self.return_type == 'full':
+            # Return full sequence
+            return seqs, mask
+        else:
+            raise ValueError(f'Unrecognized return type {self.return_type}')
--- proteoscope/modules/proteoscope.py
+++ proteoscope/modules/proteoscope.py
@@ -104,15 +104,10 @@ class ProteoscopeLM(LightningModule):
         self.results = []
 
     def forward(self, batch):
-        seq_embeds = self.esm_bottleneck(
-            batch["sequence_embed"], batch["sequence_mask"]
-        )
-
-        if torch.rand(1) < self.unconditioned_probability:
-            seq_embeds = torch.ones_like(seq_embeds)
-
-        seq_mask = torch.ones(
-            (seq_embeds.shape[0], 1), dtype=bool, device=seq_embeds.device
+        unconditioned = torch.rand(1) < self.unconditioned_probability
+        
+        seq_embeds, seq_mask = self.esm_bottleneck(
+            batch["sequence_embed"], batch["sequence_mask"], unconditioned
         )
 
         if self.cond_images:
@@ -274,11 +269,8 @@ class ProteoscopeLM(LightningModule):
         else:
             generator = None
 
-        seq_embeds = self.esm_bottleneck(
+        seq_embeds, seq_mask = self.esm_bottleneck(
             batch["sequence_embed"], batch["sequence_mask"]
-        )
-        seq_mask = torch.ones(
-            (seq_embeds.shape[0], 1), dtype=bool, device=self.unet.device
         )
 
         if cond_images is None and self.cond_images:
