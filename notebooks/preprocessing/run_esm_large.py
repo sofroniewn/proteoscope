31919b9c85e8d606ce0b842d5d9efd83afcfbd6b
sofroniewn <sofroniewn@gmail.com>
2023-09-07 17:44:11 +0000

    esm

---
+++ notebooks/preprocessing/run_esm_large.py
@@ -0,0 +1,184 @@
+import os
+import pytorch_lightning as pl
+import esm
+import zarr
+from torch.utils.data import DataLoader
+import pandas as pd
+from pytorch_lightning.strategies import FSDPStrategy
+from pytorch_lightning.callbacks import BasePredictionWriter
+from fairscale.nn.data_parallel import FullyShardedDataParallel as FSDP
+from fairscale.nn.wrap import enable_wrap, wrap
+from dataclasses import dataclass
+
+from torch.distributed.fsdp import MixedPrecision
+import torch
+from torch.utils.data.sampler import BatchSampler, SequentialSampler
+
+
+@dataclass
+class ModelConfig:
+    name: str
+    embedding_layer: int
+    embed_dim: int
+    tokens_per_batch: int
+    truncation_seq_length: int
+
+
+class ESMModel(pl.LightningModule):
+    def __init__(self, gene_to_protein, model_config):
+        super(ESMModel, self).__init__()
+        self.gene_to_protein = gene_to_protein
+        self.num_genes = len(gene_to_protein)
+        self.embedding_layer = model_config.embedding_layer  # 36
+        self.truncation_seq_length = model_config.truncation_seq_length  # 1024
+        self.tokens_per_batch = model_config.tokens_per_batch  # 1024
+        self.embed_dim = model_config.embed_dim  # 2560
+        self.model_name = model_config.name
+        self.model = None
+
+    def configure_model(self):
+        if self.model is not None:
+            return
+
+        # initialize the model with FSDP wrapper
+        fsdp_params = dict(
+            mixed_precision=True,
+            flatten_parameters=True,
+            state_dict_device=torch.device("cpu"),  # reduce GPU mem usage
+            cpu_offload=False,  # enable cpu offloading
+        )
+        with enable_wrap(wrapper_cls=FSDP, **fsdp_params):
+            model, _ = esm.pretrained.load_model_and_alphabet_hub(self.model_name)
+            # Wrap each layer in FSDP separately
+            for name, child in model.named_children():
+                if name == "layers":
+                    for layer_name, layer in child.named_children():
+                        wrapped_layer = wrap(layer)
+                        setattr(child, layer_name, wrapped_layer)
+            self.model = wrap(model)
+
+    def forward(self, x, **kwargs):
+        self.configure_model()
+        return self.model(x, **kwargs)
+
+    def predict_step(self, batch, batch_idx):
+        labels, strs, toks = batch
+        out = model(toks, repr_layers=[self.embedding_layer], return_contacts=False)
+        representations = out["representations"][self.embedding_layer]
+        return labels, strs, representations
+
+    def configure_optimizers(self):
+        pass
+
+
+class ESMDataModule(pl.LightningDataModule):
+    def __init__(self, gene_to_protein, model_config):
+        super(ESMDataModule, self).__init__()
+        # sequences = gene_to_protein['Peptide']
+        sequences = (
+            gene_to_protein["Peptide"].apply(lambda x: x.replace("*", "")).values
+        )
+        genes = gene_to_protein.index.values
+        self.num_genes = len(genes)
+
+        self.genes = genes
+        self.sequences = sequences
+        self.alphabet = None
+        self.tokens_per_batch = model_config.tokens_per_batch
+        self.truncation_seq_length = model_config.truncation_seq_length
+
+    def predict_dataloader(self):
+        dataset = esm.data.FastaBatchedDataset(self.genes, self.sequences)
+        batches = dataset.get_batch_indices(self.tokens_per_batch, extra_toks_per_seq=1)
+
+        # Use batches list directly with BatchSampler
+        batch_sampler = BatchSampler(batches, batch_size=1, drop_last=True)
+        return DataLoader(
+            dataset,
+            shuffle=False,
+            num_workers=0,
+            collate_fn=self.alphabet.get_batch_converter(self.truncation_seq_length),
+            batch_sampler=batch_sampler,
+        )
+
+
+class CustomWriter(BasePredictionWriter):
+    def __init__(self, output_dir, write_interval):
+        super().__init__(write_interval)
+        self.output_dir = output_dir
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+    def write_on_batch_end(
+        self,
+        trainer,
+        pl_module,
+        prediction,
+        batch_indices,
+        batch,
+        batch_idx,
+        dataloader_idx,
+    ):
+        # this will create N (num processes) files in `output_dir` each containing
+        # the predictions of it's respective rank
+        torch.save(
+            prediction,
+            os.path.join(
+                self.output_dir, f"predictions_{trainer.global_rank}_{batch_idx}.pt"
+            ),
+        )
+
+
+if __name__ == "__main__":
+    GENE2PROTEIN_PATH = "/home/ec2-user/cytoself-data/sequences.csv"
+    PROTEIN_EMBED_PATH = "/home/ec2-user/cytoself-data/"
+
+    # model_config = ModelConfig(
+    #     name="esm2_t33_650M_UR50D",
+    #     embedding_layer=33,
+    #     embed_dim=1280,
+    #     tokens_per_batch=1024,
+    #     truncation_seq_length=1024
+    # )
+
+    model_config = ModelConfig(
+        name="esm2_t36_3B_UR50D",
+        embedding_layer=36,
+        embed_dim=2560,
+        tokens_per_batch=1024,
+        truncation_seq_length=1024
+    )
+
+    # model_config = ModelConfig(
+    #     name="esm2_t48_15B_UR50D",
+    #     embedding_layer=48,
+    #     embed_dim=5120,
+    #     tokens_per_batch=1024,
+    #     truncation_seq_length=1024
+    # )
+
+    # Initialize DataModule and Model
+    gene_to_protein = pd.read_csv(GENE2PROTEIN_PATH)
+    gene_to_protein = gene_to_protein.append(gene_to_protein.iloc[-1]) # make divisible by 4 !!!!
+    data_module = ESMDataModule(gene_to_protein, model_config)
+    data_module.alphabet = esm.data.Alphabet.from_architecture("ESM-1b")
+    model = ESMModel(gene_to_protein, model_config)
+
+    # Use the Trainer for prediction
+    pred_writer = CustomWriter(
+        output_dir=os.path.join(PROTEIN_EMBED_PATH, model_config.name),
+        write_interval="batch",
+    )
+    strategy = FSDPStrategy(
+        cpu_offload=False, mixed_precision=MixedPrecision(param_dtype=torch.float16)
+    )
+
+    trainer = pl.Trainer(
+        devices=4,
+        callbacks=[pred_writer],
+        accelerator="cuda",
+        strategy=strategy,
+    )
+    predictions = trainer.predict(
+        model, datamodule=data_module, return_predictions=False
+    )
---
+++ notebooks/preprocessing/run_esm_small.py
@@ -0,0 +1,82 @@
+if __name__ == "__main__":
+    import esm
+    import pandas as pd
+    import torch
+    import zarr
+    from torch.utils.data import DataLoader
+    from tqdm import tqdm
+    from fairscale.nn.data_parallel import FullyShardedDataParallel as FSDP
+    from fairscale.nn.wrap import enable_wrap, wrap
+
+    GENE2PROTEIN_PATH = "/home/ec2-user/cytoself-data/sequences.csv"
+    PROTEIN_EMBED_PATH_F = (
+        "/home/ec2-user/cytoself-data/esm2_t48_15B_UR50D_sequence_embeddings_full.zarr"
+    )
+    PROTEIN_EMBED_PATH_R = "/home/ec2-user/cytoself-data/esm2_t48_15B_UR50D_sequence_embeddings_reduced.zarr"
+
+    EMBED_DIM = 1280  # 1280 or 2560
+    EMBEDDING_LAYER = 33  # 33 or 48
+    TRUNCATION_SEQ_LENGTH = 1024
+    TOKENS_PER_BATCH = 2048
+
+    # Load gene2protein mapping
+    df = pd.read_csv(GENE2PROTEIN_PATH)
+    # sequences = df['Peptide']
+    sequences = df["Peptide"].apply(lambda x: x.replace("*", "")).values
+    genes = df.index.values
+    num_genes = len(genes)
+
+    # Save locations
+    z_embedding_prot_f = zarr.open(
+        PROTEIN_EMBED_PATH_F,
+        mode="w",
+        shape=(num_genes, TRUNCATION_SEQ_LENGTH + 1, EMBED_DIM),
+        chunks=(1, None, None),
+        dtype="float32",
+    )
+
+    z_embedding_prot_r = zarr.open(
+        PROTEIN_EMBED_PATH_R,
+        mode="w",
+        shape=(num_genes, 2, EMBED_DIM),
+        chunks=(1, None, None),
+        dtype="float32",
+    )
+
+    # Load ESM-2 model
+    model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+    model.eval()  # disables dropout for deterministic results
+
+    # Create dataset
+    dataset = esm.data.FastaBatchedDataset(genes, sequences)
+    batches = dataset.get_batch_indices(TOKENS_PER_BATCH, extra_toks_per_seq=1)
+    data_loader = DataLoader(
+        dataset,
+        collate_fn=alphabet.get_batch_converter(TRUNCATION_SEQ_LENGTH),
+        batch_sampler=batches,
+    )
+
+    if torch.cuda.is_available():
+        model = model.to(device="cuda", non_blocking=True)
+
+    for batch_idx, (labels, strs, toks) in tqdm(
+        enumerate(data_loader), total=len(data_loader)
+    ):
+        if torch.cuda.is_available():
+            toks = toks.to(device="cuda", non_blocking=True)
+
+        with torch.no_grad():
+            out = model(toks, repr_layers=[EMBEDDING_LAYER], return_contacts=False)
+            representations = out["representations"][EMBEDDING_LAYER]
+
+        # Save data for each protein
+        for i, label in enumerate(labels):
+            index = df.index.get_loc(label)
+            truncate_len = min(TRUNCATION_SEQ_LENGTH, len(strs[i]))
+            bos = representations[i, 0]
+            mean = representations[i, 1 : truncate_len + 1].mean(dim=0)
+            z_embedding_prot_r[index, 0, :] = bos.detach().cpu().numpy()
+            z_embedding_prot_r[index, 1, :] = mean.detach().cpu().numpy()
+            z_embedding_prot_f[index, : truncate_len + 1] = (
+                representations[i, : truncate_len + 1].detach().cpu().numpy()
+            )
